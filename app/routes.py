e35a28d3f491972e1fead3fed44e1a422bb40446
Pchelovodova <pchelovodovaes@gmail.com>
2026-04-26 18:15:03 +0200

    Flask. Практикум 1. Создание контактной формы с использованием HTML, CSS и Flask

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, request, redirect
 
 from app import app
+
+from email_validator import validate_email, EmailNotValidError
 
 
 @app.route("/")
@@ -15,4 +17,30 @@ def about():
 
 @app.route("/contact")
 def contact():
-    return render_template("contact")
+    return render_template("contact.html")
+
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        email = request.form.get("email")
+        name = request.form.get("name")
+        message = request.form.get("message")
+        is_valid, res = email_validation(email)
+        if is_valid:
+            return render_template("contact.html", confirmation="Your message has been sent successfully!",
+                                   email=email, name=name, message=message, status="success")
+        else:
+            return render_template("contact.html", confirmation="Incorrect email. Try again", email=email,
+                                   name=name, message=message, status="error")
+    return redirect("/contact")
+
+
+def email_validation(email):
+    try:
+        is_valid = validate_email(email)
+        return True, is_valid.email
+    except EmailNotValidError:
+        return False, None
+
+
