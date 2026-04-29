fa80e53ae79934897730f96d4d4e158318bd7b1a
Pchelovodova <pchelovodovaes@gmail.com>
2026-04-29 14:14:22 +0200

    Flask. Урок 9. Основы шаблонизации в Flask

--- app/routes.py
+++ app/routes.py
@@ -3,21 +3,40 @@ from flask import render_template, request, redirect
 from app import app
 
 from email_validator import validate_email, EmailNotValidError
+from datetime import datetime
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    current_date = datetime.now()
+    return render_template("index.html", current_date=current_date)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    team_members = [
+        {'name': 'Jack', 'role': 'cook'},
+        {'name': 'Federico', 'role': 'cook'},
+        {'name': 'Tomas', 'role': 'waiter'},
+        {'name': 'Jessica', 'role': 'waiter'},
+        {'name': 'Jakub', 'role': 'waiter'},
+        {'name': 'Antonio', 'role': 'hostess'}
+
+    ]
+    return render_template("about.html", team_members=team_members)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    responsible_person = {
+        'name': 'Alex',
+        'position': 'Manager',
+        'contact': {
+            'phone': '8-123-456-789',
+            'email': 'manager@restforrest.com'
+        }
+    }
+    return render_template("contact.html", responsible_person=responsible_person)
 
 
 @app.route("/submit", methods=["POST", "GET"])
